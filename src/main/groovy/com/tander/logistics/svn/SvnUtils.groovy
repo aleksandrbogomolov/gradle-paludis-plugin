e7dc4ab82b9f059f31a55d7e7ceb427c0353f907
bogomolov_av <bogomolov_av@magnit.ru>
2017-09-21 15:26:30 +0300

    Реализована логика генерации set ebuild.

--- src/main/groovy/com/tander/logistics/svn/SvnUtils.groovy
+++ src/main/groovy/com/tander/logistics/svn/SvnUtils.groovy
@@ -1,5 +1,6 @@
 package com.tander.logistics.svn
 
+import com.tander.logistics.core.PackageVersion
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.tmatesoft.svn.core.*
@@ -111,7 +112,7 @@ class SvnUtils {
     void testConnection(String svnUrl) {
         SVNURL url = new SVNURL(svnUrl, true)
         SVNRepository repository = SVNRepositoryFactory.create(url, null);
-        repository.setAuthenticationManager(authManager);
+        repository.setAuthenticationManager(authManager)
         repository.testConnection()
     }
 
@@ -121,7 +122,7 @@ class SvnUtils {
         return svnInfo.getURL().toString()
     }
 
-    void doExportByPath(String repoUrl, String path, String filePath) throws SVNException {
+    void doImportSetByPath(String repoUrl, String path, String filePath, PackageVersion packageVersion) throws SVNException {
         DAVRepositoryFactory.setup()
         SVNRepository repository
         def out = null
@@ -129,15 +130,15 @@ class SvnUtils {
         try {
             repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(repoUrl))
             repository.setAuthenticationManager(authManager)
-            SVNNodeKind node = repository.checkPath(path, -1)
-            if (node == SVNNodeKind.NONE) {
-                logger.error("There is not file at: $repoUrl$path")
-            } else if (node == SVNNodeKind.DIR) {
-                logger.error("The entry at $repoUrl $path is a directory while a file was expected.")
+            SVNNodeKind node = repository.checkPath(path, SVNRevision.HEAD.getNumber())
+            if (node == SVNNodeKind.NONE && packageVersion.isRelease) {
+                path = findPreviousSetVersion(repository, path)
+            } else if (node == SVNNodeKind.NONE && !packageVersion.isRelease) {
+                def chainPath = (path - ".ebuild").split("-")
+                path = "${chainPath[0]}-${chainPath[1]}-${chainPath[2]}-${chainPath[3].substring(0, chainPath[3].lastIndexOf("."))}.ebuild"
             }
-            println(node.toString())
             out = new ByteArrayOutputStream()
-            repository.getFile(path, -1, new SVNProperties(), out)
+            repository.getFile(path, SVNRevision.HEAD.getNumber(), new SVNProperties(), out)
             out.writeTo(fos)
         } catch (SVNException e) {
             logger.error(e.errorMessage.toString())
@@ -151,29 +152,27 @@ class SvnUtils {
         }
     }
 
-    static SVNRevision getSvnRevision(String revision) {
-        switch (revision) {
-            case 'HEAD':
-                return SVNRevision.HEAD
-                break
-            case 'WORKING':
-                return SVNRevision.WORKING
-                break
-            case 'PREVIOUS':
-                return SVNRevision.PREVIOUS
-                break
-            case 'BASE':
-                return SVNRevision.BASE
-                break
-            case 'COMMITTED':
-                return SVNRevision.COMMITTED
-                break
-            case 'UNDEFINED':
-                return SVNRevision.UNDEFINED
-                break
-            default:
-                return SVNRevision.create(revision as long)
-                break
+    String findPreviousSetVersion(SVNRepository repository, String path) {
+        String result = path
+        def regex = ~/\d*\.\d*\.\d*/
+        def dir = new ArrayList<SVNDirEntry>()
+        repository.getDir(".", SVNRevision.HEAD.getNumber(), new SVNProperties(), dir)
+        def ebuildNames = new ArrayList()
+        ebuildNames.add(path)
+        dir.each { f ->
+            def name = f.getName() - ".ebuild"
+            def chainName = name.split("-")
+            if (regex.matcher(chainName.last()).matches()) {
+                ebuildNames.add(name)
+            }
+        }
+        ebuildNames.sort()
+        ebuildNames.each { e ->
+            if (e == path) {
+                return result
+            } else {
+                result = e
+            }
         }
     }
 }
--- src/main/groovy/com/tander/logistics/tasks/PaludisPackageDistributionTask.groovy
+++ src/main/groovy/com/tander/logistics/tasks/PaludisPackageDistributionTask.groovy
@@ -19,15 +19,19 @@ import org.tmatesoft.svn.core.wc.SVNRevision
  */
 class PaludisPackageDistributionTask extends DefaultTask {
 
+    String svnSetPath = 'https://sources.corp.tander.ru/svn/real_out/pkg/repository/set/'
+    def parentEbuild = "$project.buildDir.path/parentEbuild.ebuild"
+
     PaludisPackageExtension ext
     SvnUtils svnUtils
     SvnBranchAbstract currBranch
     SvnBranchAbstract prevBranch
     PackageVersion packageVersion
-
     boolean doCheckSVN
+
     LinkedHashMap<String, List<String>> wildcards
     Map<String, Boolean> paludisPackages = new HashMap<>()
+    Map<String, String> packages = new HashMap<>()
 
     PaludisPackageDistributionTask() {
         group = "distribution"
@@ -87,7 +91,7 @@ class PaludisPackageDistributionTask extends DefaultTask {
                 for (Map.Entry<String, List<String>> entry : wildcards.entrySet()) {
                     for (wildcard in entry.value)
                         if (FilenameUtils.wildcardMatch(file, wildcard as String)) {
-                            paludisPackages.put(entry.key, true)
+                            addToPaludisPackages(entry.key)
                             isMatched = true
                             break
                         }
@@ -98,12 +102,17 @@ class PaludisPackageDistributionTask extends DefaultTask {
             }
         } else {
             for (Map.Entry<String, List<String>> entry : wildcards.entrySet()) {
-                paludisPackages.put(entry.key, true)
+                addToPaludisPackages(entry.key)
             }
         }
 
         generatePackageVersion()
         generateEbuild()
+        generateSetEbuild()
+    }
+
+    void addToPaludisPackages(String key) {
+        paludisPackages.put(key, true)
     }
 
     void generatePackageVersion() { // TODO Добавить проверку версии по паттерну
@@ -143,8 +152,8 @@ class PaludisPackageDistributionTask extends DefaultTask {
         def destinationDir = new File(project.buildDir, "ebuilds")
         if (!destinationDir.exists()) {
             destinationDir.mkdirs()
-            println(destinationDir.exists())
         }
+//        project.tasks.findByName("paludisPackageDistribution").property("paludisPackages") as HashMap
         paludisPackages.each { key, value ->
             if (value) {
                 new File(destinationDir, "$ext.packageName-$key-${packageVersion.version}.ebuild").write(new File("template/$key").text, "UTF-8")
@@ -153,17 +162,42 @@ class PaludisPackageDistributionTask extends DefaultTask {
     }
 
     def generateSetEbuild() {
-        def setUrl = "https://sources.corp.tander.ru/svn/real_out/pkg/repository/set/$ext.setName"
-        svnUtils.doExportByPath(setUrl, "/tomcatsrv-rc-web-1.148.10.ebuild", "$project.buildDir.path/tmp")
-//        svnUtils.doExport("$setUrl/tomcatsrv-rc-web-1.148.1000.ebuild", "$project.buildDir.path/tmp", SVNRevision.HEAD, new ISVNEventHandler() {
-//            @Override
-//            void handleEvent(SVNEvent event, double progress) throws SVNException {
-//                logger.lifecycle("Exporting file " + event.getFile().toString())
-//            }
-//
-//            @Override
-//            void checkCancelled() throws SVNCancelException {
-//            }
-//        })
+        svnUtils.doImportSetByPath("$svnSetPath$ext.setName", "$ext.setName-${packageVersion.version}.ebuild", parentEbuild, packageVersion)
+        if (new File(parentEbuild).text == "") {
+            new File(parentEbuild).text = new File("template/set").text
+            wildcards.keySet().each { k ->
+                addToPaludisPackages(k)
+            }
+        }
+        doNewSetEbuild()
+    }
+
+    void doNewSetEbuild() {
+        def packageList = new ArrayList<String>()
+        wildcards.each { k, v ->
+            packageList.add("$ext.setName-$k-$packageVersion.version")
+        }
+        paludisPackages.each { k, v ->
+            packageList.each { tbz ->
+                if (tbz.contains(k)) {
+                    packages.put(k, tbz)
+                }
+            }
+        }
+        StringBuilder text = new StringBuilder("")
+        new File(parentEbuild).eachLine { line ->
+            text.append(checkLine(line))
+        }
+        new File("$project.buildDir.path/ebuilds/${ext.setName}.${packageVersion.version}.ebuild").write(text.toString(), 'UTF-8')
+    }
+
+    String checkLine(String line) {
+        String result = "$line\n"
+        packages.each { k, v ->
+            if (line.contains(k)) {
+                result = "${line.substring(0, line.indexOf('/') + 1)}$v\n"
+            }
+        }
+        return result
     }
 }
